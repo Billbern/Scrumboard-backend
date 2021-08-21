b6f92da7c9c225fe52cc15f4e4f8608810bf5775
Billbern <denis.abaidoo@gmail.com>
2021-08-21 16:27:20 +0000

    added a logs route

--- routes/multidata.js
+++ routes/multidata.js
@@ -7,6 +7,7 @@ route.use(express.json());
 
 // get document model for accessing data
 const Task = require('../models/task');
+const Logs = require('../models/log');
 const verifyToken = require('../utils/verifytoken');
 
 // handle read requests for all tasks
@@ -26,5 +27,21 @@ route.get('/tasks', verifyToken, async (req, res) => {
 }
 })
 
+
+route.get('/logs', verifyToken, async (req, res) => {
+    try {
+        const verifyUser = await jwt.verify(req.params.token, process.env.SECRET_KEY);
+        if (verifyUser) {
+            let logs = await Logs.find({ owner: verifyUser.id }).populate('task', 'id');
+            res.status(200).json(logs);
+        } else {
+            res.clearCookie('ahyensew');
+            res.status(403).json("User not authorized")
+        }
+    } catch (err) {
+    console.error(err);
+    res.status(500).json("Error")
+}
+})
 
 module.exports = route;
